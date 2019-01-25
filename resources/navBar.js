1b514a0d8cb687af8a093b38ae905f633fd1955b
dfsek <39248502+dfsek@users.noreply.github.com>
2019-01-25 08:47:04 -0700

    added more places

--- resources/navBar.js
+++ resources/navBar.js
@@ -5,7 +5,7 @@ if(window.location.port === "" || !window.location.port) {
 	baseUrl = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port;
 }
 console.log("base URL: " + baseUrl);
-var navBarHTML = '<div id="mySidenav" class="sidenav"> <a href="#" class="closebtn" onclick="closeNav()">&times;</a> <a href="' + baseUrl + '">Pagina Principal</a> <a href="' + baseUrl + '/lugares">Lugares</a><a href="' + baseUrl + '/itinerarios">Itinerarios</a><a href="' + baseUrl + '/conclusion">Conclusion</a></div>';
+var navBarHTML = '<div id="mySidenav" class="sidenav"> <a href="#" class="closebtn" onclick="closeNav()">&times;</a> <a href="' + baseUrl + '">Pagina Principal</a> <a href="' + baseUrl + '/lugares">Lugares</a><a href="' + baseUrl + '/lugares/sancristobal"><div class="navBarPadding">La Colina de San Cristóbal</div></a><a href="' + baseUrl + '/lugares/sanlucia"><div class="navBarPadding">La Colina de San Lucia</div></a><a href="' + baseUrl + '/itinerarios">Itinerarios</a><a href="' + baseUrl + '/conclusion">Conclusion</a></div>';
 
 
 $(document).ready(function() {
