87cfe7b3381ff4da8aad45c4d4f64205281f9165
dfsek <39248502+dfsek@users.noreply.github.com>
2019-01-28 08:46:41 -0700

    added things

--- resources/navBar.js
+++ resources/navBar.js
@@ -5,7 +5,7 @@ if(window.location.port === "" || !window.location.port) {
 	baseUrl = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port;
 }
 console.log("base URL: " + baseUrl);
-var navBarHTML = '<div id="mySidenav" class="sidenav"> <a href="#" class="closebtn" onclick="closeNav()">&times;</a> <a href="' + baseUrl + '">Pagina Principal</a> <a href="' + baseUrl + '/lugares">Lugares</a><a href="' + baseUrl + '/lugares/sancristobal"><div class="navBarPadding">La Colina de San Cristóbal</div></a><a href="' + baseUrl + '/lugares/sanlucia"><div class="navBarPadding">La Colina de San Lucia</div></a><a href="' + baseUrl + '/itinerarios">Itinerarios</a><a href="' + baseUrl + '/conclusion">Conclusion</a></div>';
+var navBarHTML = '<div id="mySidenav" class="sidenav"> <a href="#" class="closebtn" onclick="closeNav()">&times;</a> <a href="' + baseUrl + '">Pagina Principal</a> <a href="' + baseUrl + '/lugares">Lugares</a><a href="' + baseUrl + '/lugares/sancristobal"><div class="navBarPadding">La Colina de San Cristóbal</div></a><a href="' + baseUrl + '/lugares/mercadocentral"><div class="navBarPadding">El Mercado Central de Santiago</div></a><a href="' + baseUrl + '/lugares/sanlucia"><div class="navBarPadding">La Colina de San Lucia</div></a><a href="' + baseUrl + '/lugares/plazadearmas"><div class="navBarPadding">La Plaza de Armas</div></a><a href="' + baseUrl + '/lugares/parque"><div class="navBarPadding">El Parque Metropolitano</div></a><a href="' + baseUrl + '/lugares/zoo"><div class="navBarPadding">El Zoológico de Santiago</div></a><a href="' + baseUrl + '/itinerarios">Itinerarios</a><a href="' + baseUrl + '/conclusion">Conclusion</a></div>';
 
 
 $(document).ready(function() {
